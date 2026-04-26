2851b38e6c9a220717f5d4f03b059f5f44702bf7
Thomas Kirz <thomas.kirz@gmail.com>
2026-04-26 18:00:19 +0200

    download checker framework in container

--- scripts/buildOpsc.sh
+++ scripts/buildOpsc.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+echo "Downloading Checker Framework..."
+cd /artifact/src
+git clone https://github.com/eisop/checker-framework.git
+cd checker-framework
+git checkout 323f434a07169d967c1a14579b7849f714aebdd5
+echo "Publishing Checker Framework (3.49.5-eisop1) to local Maven cache..."
+./gradlew publishToMavenLocal
+
 echo "Publishing OPSC to local Maven cache..."
 cd /artifact/src/opsc
 ./gradlew publishToMavenLocal

8dfba37589862b1b4654f4579561784febeba34d
Thomas Kirz <thomas.kirz@gmail.com>
2026-05-04 11:53:24 +0200

    opennms prerun only once

--- projects/opennms/prerun.sh
+++ projects/opennms/prerun.sh
@@ -3,22 +3,27 @@
 scriptDir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 cd $scriptDir
 
-for dir in opennms-noannos opennms-annos opennms-value; do
-    (
-    echo "Assembling project in $dir with ./compile.pl..."
-    cd $dir
-    docker compose up -d
-    ./compile.pl -DskipTests=true -PskipCheckerFramework --projects :opennms-webapp -am install
-    docker compose down
-    )
-done
 
-for dir in opennms-nocf; do
-    (
-    echo "Assembling project in $dir with ./compile.pl..."
-    cd $dir
-    docker compose up -d
-    ./compile.pl -DskipTests=true --projects :opennms-webapp -am install
-    docker compose down
-    )
-done
+dir="opennms-nocf"
+
+echo "Assembling project in $dir with ./compile.pl..."
+cd $dir
+
+
+# --- FIX: Calculate the Host Path ---
+# 1. Get the path relative to the container root (/artifact)
+#    Inside container: /artifact/opsc/opennms
+#    We want: opsc/opennms
+REL_PATH=$(realpath --relative-to="/artifact" "$(pwd)")
+
+# 2. Combine Host Root + Relative Path
+export DB_MOUNT_PATH="$HOST_PROJECT_ROOT/$REL_PATH"
+
+echo "Host Project Root: $HOST_PROJECT_ROOT"
+echo "Relative Path: $REL_PATH"
+echo "Mounting Host Path: $DB_MOUNT_PATH"
+
+
+docker compose up -d --wait
+./compile.pl -DskipTests=true --projects :opennms-webapp -am install
+docker compose down
